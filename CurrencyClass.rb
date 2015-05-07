6eb236bec068d63f35e92ba242c21576a44ab408
Anna Bedell <awbedell@gmail.com>
2015-05-07 13:47:00 -0400

    Initialize with one argument and the class splits it into code and amount.

--- CurrencyClass.rb
+++ CurrencyClass.rb
@@ -1,6 +1,21 @@
-class Currency(code, amount)
-  def initialize
-    @code = code
-    @amount = amount.to_f
+class Currency
+  def initialize(entry)
+    @code = ""
+    while entry[0].to_i.to_s != entry[0]
+      @code << entry[0]
+      entry[0] = ''
+    end
+    @amount = entry.to_f
+  end
+  def code
+    @code
+  end
+  def amount
+    @amount
   end
 end
+
+
+try = Currency.new("$2.05")
+puts try.code
+puts try.amount
