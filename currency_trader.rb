3673fa3cde98db2b606749eb4cb8740b13401878
Anna Bedell <awbedell@gmail.com>
2015-05-08 15:03:36 -0400

    Trader accepts arrays

--- currency_trader.rb
+++ currency_trader.rb
@@ -2,26 +2,25 @@ require "./currency.rb"
 require "./currency_converter.rb"
 
 class CurrencyTrader
-  def initialize(first, second, origin)
-    @first = first #origin
-    @second = second #destination
+  def initialize(converters, origin)
+    @converters = converters
     @origin = origin
   end
 
   def best_trade
     best_ratio = 0
     best_trade = ""
-    @first.hash.each_value do |rate|
-      if ((rate/@second.hash[@first.hash.key(rate)]) <=> best_ratio) == 1
-        best_ratio = rate/@second.hash[@first.hash.key(rate)]
-        best_trade = @first.hash.key(rate)
+    @converters[0].hash.each_value do |rate|
+      if ((rate/@converters[1].hash[@converters[0].hash.key(rate)]) <=> best_ratio) == 1
+        best_ratio = rate/@converters[1].hash[@converters[0].hash.key(rate)]
+        best_trade = @converters[0].hash.key(rate)
       end
     end
     return best_trade
   end
 
   def should_i_trade(destination)
-    if (@first.hash[destination]/@second.hash[destination]) > 1
+    if (@converters[0].hash[destination]/@converters[1].hash[destination]) > 1
       true
     else
       false
