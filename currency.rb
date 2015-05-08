dd044ba2dfbb4f5068ed64df7cc9fdec98f45d38
Anna Bedell <awbedell@gmail.com>
2015-05-08 09:45:48 -0400

    File name fix

---
+++ currency.rb
@@ -0,0 +1,77 @@
+class Currency
+  def initialize(entry, optional_amount = "000.00")
+    @codes = {
+      '$' => 'USD' , '€' => 'EUR', '¥' => 'JPY', '£' => 'GBP', '₽' => 'RUB',
+      '₨' => 'INR', '₩' => 'KRW', '₦' => 'NGN', '₴' => 'UAH', '₭' => 'LAK',
+      '₱' => 'PHP', '₮' => 'MNT', '₺' => 'TRY', '฿' => 'THB', '﷼' => 'SAR',
+      '₹' => 'INR', '₲' => 'PYG', '₡' => 'CRC', '₸' => 'KZT', '₫' => 'VND' }
+    if optional_amount == "000.00"
+      @code = ""
+      while entry[0].to_i.to_s != entry[0]
+        @code << entry[0]
+        entry[0] = ''
+        @code.strip!
+      end
+      if @code.length != 3
+        @code = @codes[@code]
+      end
+      @amount = entry.to_f
+    else
+      @code = entry
+      @amount = optional_amount.to_f
+    end
+  end
+
+  def code
+    @code
+  end
+
+  def amount
+    @amount
+  end
+
+  def == (other_currency)
+    true if @code == other_currency.code && @amount == other_currency.amount
+  end
+
+  def <=> (other_currency)
+    if @amount < other_currency.amount
+      -1
+    elsif @amount > other_currency.amount
+      1
+    else
+      0
+    end
+  end
+
+  def - (other_currency)
+    if @code != other_currency.code
+      raise DifferentCurrencyCodeError
+    else
+      Currency.new(@code, @amount - other_currency.amount)
+    end
+  end
+
+  def + (other_currency)
+    if @code != other_currency.code
+      raise DifferentCurrencyCodeError
+    else
+      Currency.new(@code, @amount + other_currency.amount)
+    end
+  end
+
+  def * (number)
+    Currency.new(@code, @amount*number)
+  end
+
+  def / (number)
+    Currency.new(@code, @amount/number)
+  end
+
+end
+
+class DifferentCurrencyCodeError < StandardError
+  def message
+    "You can only add and subtract equivalent currencies."
+  end
+end
---
+++ currency_converter.rb
@@ -0,0 +1,25 @@
+require "./CurrencyClass.rb"
+
+class CurrencyConverter
+  def initialize(origin, destination)
+    @origin = origin
+    @destination = destination
+    @conversion_rates_to_USD = {'USD' => 1, 'EUR' => 0.887, 'JPY' => 119.822,
+      'GBP' => 0.647, 'RUB' => 50.775, 'INR' => 63.927, 'KRW' => 1091.49,
+      'NGN' => 199.049, 'UAH' => 20.49, 'LAK' => 8088.57, 'PHP' => 44.597,
+      'MNT' => 1947.33, 'TRY' => 2.677, 'THB' => 33.458, 'SAR' => 3.75,
+      'PYG' => 5047.19, 'CRC' => 532.203, 'KZT' => 185.88, 'VND' => 21691.44} # As of May, 8 2015
+  end
+
+  def convert
+    Currency.new(@destination, (@conversion_rates_to_USD[@destination] /
+    @conversion_rates_to_USD[@origin.code]) * @origin.amount)
+  end
+end
+
+first = Currency.new("USD2.00")
+
+testit = CurrencyConverter.new(first, 'EUR')
+
+puts testit.convert.amount
+puts testit.convert
