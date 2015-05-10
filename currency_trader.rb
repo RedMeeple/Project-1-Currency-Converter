c14498f9bdb85a74c5b8a6df8aeacb240fe232dd
Anna Bedell <awbedell@gmail.com>
2015-05-10 09:42:48 -0400

    progress on trading through multiple time periods. Revert amount not calculating correctly

--- currency_trader.rb
+++ currency_trader.rb
@@ -10,7 +10,7 @@ class CurrencyTrader
   def best_trade
     raise MissingCurrencyError if @converters.length < 2
 
-    best_value = 0
+    best_value = @origin
     best_trade = ''
     start = @origin
     spot = 0
@@ -18,19 +18,27 @@ class CurrencyTrader
 
     until spot == @converters.length-1
       @converters[spot].hash.each_key do |country|
+        puts "In hash key: " + country + ", " + start.code
         trade = CurrencyConverter.new(@converters[spot].hash)
         trade_amount = trade.convert(start, country)
+        mature_amount = trade_amount * (@converters[spot+1].hash[country] / @converters[spot].hash[country])
+        puts mature_amount.code
         mature = CurrencyConverter.new(@converters[spot+1].hash)
-        mature_amount = mature.convert(trade_amount, country)
-        revert_amount = mature.convert(mature_amount, @origin.code)
-        if (revert_amount <=> @origin) == 1
+        revert_amount = mature.convert(mature_amount, start.code)
+        ## revert_amount not calculating correctly
+        puts "RA: " + revert_amount.amount.to_s
+        if (revert_amount <=> best_value) == 1
           best_value = revert_amount
+          puts "BV: " + best_value.amount.to_s
           best_trade = country
         end
       end
       spot += 1
+      puts spot
       start = best_value
+      puts start.amount
       trade_route << best_trade
+      puts trade_route
     end
     "If you trade to #{trade_route} you will end up with #{best_value.code} #{best_value.amount}"
   end
